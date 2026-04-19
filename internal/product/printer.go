a090218c24a6f9075fb8374fd6c16d978215d316
Shuhrat <shukhrat.shokirov@alif.uz>
2026-04-19 21:42:50 +0500

    fix tests

--- internal/product/printer.go
+++ internal/product/printer.go
@@ -1,6 +1,7 @@
 package product
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 )
@@ -22,11 +23,46 @@ const (
 ====================`
 )
 
+// formatMoney форматирует сумму в тийинах в читаемый вид
+func formatMoney(tiyin int) string {
+	// Переводим тийины в сумы
+	sum := tiyin / 100
+	tiynRemainder := tiyin % 100
+
+	// Преобразуем целую часть в строку
+	sumStr := strconv.Itoa(sum)
+
+	// Добавляем разделители тысяч
+	formattedSum := addThousandSeparators(sumStr)
+
+	// Добавляем тийины только если они есть
+	if tiynRemainder > 0 {
+		return fmt.Sprintf("%s.%02d", formattedSum, tiynRemainder)
+	}
+	return formattedSum
+}
+
+// addThousandSeparators добавляет пробелы между разрядами
+func addThousandSeparators(s string) string {
+	if len(s) <= 3 {
+		return s
+	}
+
+	var result []byte
+	for i, ch := range s {
+		if i > 0 && (len(s)-i)%3 == 0 {
+			result = append(result, ' ')
+		}
+		result = append(result, byte(ch))
+	}
+	return string(result)
+}
+
 func Converter(product Product, firstAmount int) string {
 	text := strings.ReplaceAll(template, "{name}", product.Name)
 	text = strings.ReplaceAll(text, "{brand}", product.Brand)
-	text = strings.ReplaceAll(text, "{price}", strconv.Itoa(product.Price))
+	text = strings.ReplaceAll(text, "{price}", formatMoney(product.Price))
 	text = strings.ReplaceAll(text, "{inStock}", strconv.FormatBool(product.InStock))
-	text = strings.ReplaceAll(text, "{firstAmount}", strconv.Itoa(firstAmount))
+	text = strings.ReplaceAll(text, "{firstAmount}", formatMoney(firstAmount))
 	return text
 }
